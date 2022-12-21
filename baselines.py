19b3012012545406d39ced5ea15cd25be6cdc555
Rasmus Rynell <rynell.rasmus@gmail.com>
2022-12-21 15:55:46 +0100

    Restructure

--- baselines.py
+++ baselines.py
@@ -3,86 +3,98 @@ import random
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import Ridge
-from sklearn.svm import SVR
 from sklearn.linear_model import LogisticRegression
+from sklearn.dummy import DummyClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+from sklearn.datasets import make_classification
+from sklearn.metrics import plot_confusion_matrix
+import time
 
 threads = 8
 
 import common
-idx = 1
-df, train_df, validation_df, test_df = common.get_dataset(idx)
-
-# Reset all indexes
-train_df = train_df.reset_index(drop=True)
-validation_df = validation_df.reset_index(drop=True)
-test_df = test_df.reset_index(drop=True)
 
 
-# Debug, print CountVectorizer of first text
-test_idx = 1
-print(train_df.iloc[test_idx]['x'], flush=True)
 
 
-# Scale the data
-# y_scaler = StandardScaler()
-# y_scaler.fit(train_df['y'].values.reshape(-1, 1))
-# train_df['y'] = y_scaler.transform(train_df['y'].values.reshape(-1, 1))
-# validation_df['y'] = y_scaler.transform(validation_df['y'].values.reshape(-1, 1))
-# test_df['y'] = y_scaler.transform(test_df['y'].values.reshape(-1, 1))
 
 
-# Bag of words
-# Baseline 1: Linear regression
-linear_regression = Pipeline([('vect', CountVectorizer()), ('model', LinearRegression())])
-linear_regression.fit(train_df['x'], train_df['y'])
-y_pred = linear_regression.predict(validation_df['x'])
-mse = ((validation_df['y'] - y_pred) ** 2).mean()
-print(f'Linear regression MSE:\t {mse:.2f}', flush=True)
-print(f'{y_pred[test_idx]:.2f}, {validation_df["y"][test_idx]:.2f} = {y_pred[test_idx] - validation_df["y"][test_idx]:.2f}', flush=True)
+def run_evaluate_and_save_pipeline(pipeline, train_df, validation_df, test_df, name):
+    # Train the pipeline
+    train_time_start = time.time()
+    pipeline.fit(train_df['x'], train_df['y'])
+    train_time_end = time.time()
 
-# Baseline 2: Ridge regression
-ridge_regression = Pipeline([('vect', CountVectorizer()), ('model', Ridge())])
-ridge_regression.fit(train_df['x'], train_df['y'])
-y_pred = ridge_regression.predict(validation_df['x'])
-mse = ((validation_df['y'] - y_pred) ** 2).mean()
-print(f'Ridge regression MSE:\t {mse:.2f}', flush=True)
-print(f'{y_pred[test_idx]:.2f}, {validation_df["y"][test_idx]:.2f} = {y_pred[test_idx] - validation_df["y"][test_idx]:.2f}', flush=True)
+    # Evaluate the pipeline
+    va_score = pipeline.score(validation_df['x'], validation_df['y'])
+    test_score = pipeline.score(test_df['x'], test_df['y'])
+    print(f'{name} validation score: {va_score}', flush=True)
+    print(f'{name} test score: {test_score}', flush=True)
 
-print()
+    # More metrics
+    y_true = test_df['y']
+    y_pred = pipeline.predict(test_df['x'])
+    cm = confusion_matrix(y_true, y_pred)
+    print(f'{name} confusion matrix: \n{cm}', flush=True)
 
-# TF-IDF
-# Baseline 4: Linear regression
-linear_regression = Pipeline([('vect', TfidfVectorizer()), ('model', LinearRegression())])
-linear_regression.fit(train_df['x'], train_df['y'])
-y_pred = linear_regression.predict(validation_df['x'])
-mse = ((validation_df['y'] - y_pred) ** 2).mean()
-print(f'Linear regression MSE:\t {mse:.2f}', flush=True)
-print(f'{y_pred[test_idx]:.2f}, {validation_df["y"][test_idx]:.2f} = {y_pred[test_idx] - validation_df["y"][test_idx]:.2f}', flush=True)
+    print()
 
-# Baseline 5: Ridge regression
-ridge_regression = Pipeline([('vect', TfidfVectorizer()), ('model', Ridge())])
-ridge_regression.fit(train_df['x'], train_df['y'])
-y_pred = ridge_regression.predict(validation_df['x'])
-mse = ((validation_df['y'] - y_pred) ** 2).mean()
-print(f'Ridge regression MSE:\t {mse:.2f}', flush=True)
-print(f'{y_pred[test_idx]:.2f}, {validation_df["y"][test_idx]:.2f} = {y_pred[test_idx] - validation_df["y"][test_idx]:.2f}', flush=True)
-
-print()
-
-# Print text for largest y_true
-print('Largest y_true:')
-print(train_df.iloc[train_df['y'].idxmax()]['x'], flush=True)
-print(validation_df.iloc[validation_df['y'].idxmax()]['x'], flush=True)
-print(test_df.iloc[test_df['y'].idxmax()]['x'], flush=True)
+    # Save the results
+    # common.save_results(name, pipeline, train_time_end - train_time_start, va_score, test_score, cm)
 
 
-# Show a bar chart of y_true for train, validation and test in different colors
-plt.bar(range(len(train_df)), train_df['y'], color='b', label='train')
-plt.bar(range(len(train_df), len(train_df) + len(validation_df)), validation_df['y'], color='r', label='validation')
-plt.bar(range(len(train_df) + len(validation_df), len(train_df) + len(validation_df) + len(test_df)), test_df['y'], color='g', label='test')
-plt.legend()
-plt.show()
+
+idx = 0
+for idx in range(0, 2):
+    dataset_name, others = common.get_dataset(idx)
+    df, train_df, validation_df, test_df = others
+
+    # Reset all indexes
+    train_df = train_df.reset_index(drop=True)
+    validation_df = validation_df.reset_index(drop=True)
+    test_df = test_df.reset_index(drop=True)
+
+    # Print how long the train, validation and test sets are
+    print(f'Train set length: {len(train_df)}', flush=True)
+    print(f'Validation set length: {len(validation_df)}', flush=True)
+    print(f'Test set length: {len(test_df)}', flush=True)
+
+    # Print how many unique values there are in y_true
+    print(f'Unique values in y_true: {len(train_df["y"].unique())}', flush=True)
+    # Print how many of each unique value there are in y_true
+    print(f'Unique values in y_true: {train_df["y"].value_counts().to_dict()}', flush=True)
+
+
+
+    # Bag of words
+    # Baseline 1: Dummy classifier
+    pipeline = Pipeline([
+        ('vectorizer', CountVectorizer()),
+        ('classifier', DummyClassifier(strategy='most_frequent'))
+    ])
+    run_evaluate_and_save_pipeline(pipeline, train_df, validation_df, test_df, f'{dataset_name}: BOW - Dummy classifier')
+
+    # Baseline 2: MultinomialNB
+    pipeline = Pipeline([
+        ('vectorizer', CountVectorizer()),
+        ('classifier', MultinomialNB())
+    ])
+    run_evaluate_and_save_pipeline(pipeline, train_df, validation_df, test_df, f'{dataset_name}: BOW - MultinomialNB')
+
+    # TF-IDF
+    # Baseline 4: Dummy classifier
+    pipeline = Pipeline([
+        ('vectorizer', TfidfVectorizer()),
+        ('classifier', DummyClassifier(strategy='most_frequent'))
+    ])
+    run_evaluate_and_save_pipeline(pipeline, train_df, validation_df, test_df, f'{dataset_name}: TF-IDF - Dummy classifier')
+
+    # Baseline 5: MultinomialNB
+    pipeline = Pipeline([
+        ('vectorizer', TfidfVectorizer()),
+        ('classifier', MultinomialNB())
+    ])
+    run_evaluate_and_save_pipeline(pipeline, train_df, validation_df, test_df, f'{dataset_name}: TF-IDF - MultinomialNB')
--- common.py
+++ common.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import StandardScaler
 import torch
 import numpy as np
 import random
+from datasets import load_dataset
 
 seed = 41
 torch.manual_seed(seed)
@@ -15,33 +16,35 @@ random.seed(seed)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
+
 data = [{
-        'name': 'movie',
-        'address': 'akshaypawar7/millions-of-movies',
-        'file': 'movies.csv',
-        'x_column': 'overview',
-        'y_column': 'budget'
+        'name': 'IMDB',
+        'address': 'lakshmi25npathi/imdb-dataset-of-50k-movie-reviews',
+        'file': 'IMDB_Dataset.csv',
+        'x_column': 'review',
+        'y_column': 'sentiment',
+        'type': 'classification',
+        'link': 'https://www.kaggle.com/datasets/lakshmi25npathi/imdb-dataset-of-50k-movie-reviews'
         },
         {
-        'name': 'musk',
-        'address': 'marta99/elon-musks-tweets-dataset-2022',
-        'file': 'cleandata.csv',
-        'x_column': 'Cleaned_Tweets',
-        'y_column': 'Likes'
-        }]
-
-try:
-    kaggle.api.authenticate()
-    for dataset in data:
-        if not os.path.exists(f"data/{dataset['name']}") or \
-            not os.path.exists(f"data/{dataset['name']}/{dataset['file']}"):
-            print(f"Downloading {dataset['name']} dataset", flush=True)
-            kaggle.api.dataset_download_files(dataset['address'], \
-                path=f"data/{dataset['name']}", unzip=True)
-except:
-    print("Kaggle API not working, either download the data manually or use the Kaggle CLI")
-    for dataset in data:
-        print(f"Download {dataset['name']} dataset from https://www.kaggle.com/{dataset['address']} and place its content it in data/{dataset['name']} folder")
+        'name': 'Fake_News',
+        'address': 'hassanamin/textdb3',
+        'file': 'fake_or_real_news.csv',
+        'x_column': 'text',
+        'y_column': 'label',
+        'type': 'classification',
+        'link': 'https://www.kaggle.com/hassanamin/textdb3'
+        },
+        {
+        'name': 'Disneyland_Reviews',
+        'address': 'arushchillar/disneyland-reviews',
+        'file': 'disneyland_reviews.csv',
+        'x_column': 'review',
+        'y_column': 'sentiment',
+        'type': 'multi-classification',
+        'link': 'https://www.kaggle.com/arushchillar/disneyland-reviews'
+        }
+        ]
 
 
 def clean_up_dataset(df, df_col_x, df_col_y, drop_zeros=False):
@@ -68,32 +71,22 @@ def split_data(df, test_size, validation_size, seed):
     validation_df, test_df = train_test_split(test_val_df, \
         train_size=validation_size/(test_size+validation_size), random_state=seed)
     
-    return train_df, validation_df, test_df
+    return df, train_df, validation_df, test_df
 
 
 def get_dataset(idx):
     curr_data = data[idx]
-    print(f"Using {curr_data['name']} dataset")
+    print(f"Using {curr_data['name']} dataset", flush=True)
 
-    # Read data
-    text_column = curr_data['x_column']
-    ans_column = curr_data['y_column']
     df = pd.read_csv(f'data/{curr_data["name"]}/{curr_data["file"]}', encoding='utf8')
-    df = clean_up_dataset(df, text_column, ans_column, drop_zeros=True)
-
-    # Rename columns
-    df = df.rename(columns={text_column: 'x', ans_column: 'y'})
-
-    # Shuffle the data
+    #df = clean_up_dataset(df, curr_data['x_column'], curr_data['y_column'], drop_zeros=True)
+    df = df.rename(columns={curr_data['x_column']: 'x', curr_data['y_column']: 'y'})
     df = df.sample(frac=1, random_state=seed)
 
+    test_size = 0.1
+    validation_size = 0.1
 
-    train_df, validation_df, test_df = split_data(df, test_size=0.2, validation_size=0.2, seed=seed)
-    print(f"Train set size: \t{len(train_df)} ({len(train_df)/len(df)*100:.2f}%)", flush=True)
-    print(f"Validation set size: \t{len(validation_df)} ({len(validation_df)/len(df)*100:.2f}%)", flush=True)
-    print(f"Test set size: \t\t{len(test_df)} ({len(test_df)/len(df)*100:.2f}%)", flush=True)
-
-    return df, train_df, validation_df, test_df
+    return curr_data['name'], split_data(df, test_size, validation_size, seed)
 
 def save_results():
     pass
