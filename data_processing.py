53731dd88038961f3f50b30d30559d7b6583dcec
Rasmus Rynell <rynell.rasmus@gmail.com>
2023-03-12 18:50:31 +0100

    removed prints

--- data_processing.py
+++ data_processing.py
@@ -7,18 +7,10 @@ def read_clean_data():
     # Line (17301 in csv / 17299 in df) is corrupted, just remove it
     data = data.drop([17299])
 
-    print(data.columns)
-
     # Drop columns that are not needed
     data = data.drop(['ProductPrice', 'Review', 'Rate', 'ProductName'], axis=1)
 
-    # Remember how many rows we have
-    old_len = len(data)
-
     data = data.dropna()
-
-    # Check if we have dropped any rows
-    print(f"Removed {old_len - len(data)} rows with NaN values")
 
     # Convert to same values
     data['Sentiment'] = data['Sentiment'].replace(['Positive', 'Negative', 'Neutral'], ['positive', 'negative', 'neutral'])
@@ -26,13 +18,8 @@ def read_clean_data():
     # Convert to ints
     data['Sentiment'] = data['Sentiment'].replace(['positive', 'neutral', 'negative'], [2, 1, 0])
 
-    old_len = len(data)
     remove_len = 450
     data = data[data['Summary'].str.len() < remove_len]
-    print(f"Removed {old_len - len(data)} rows with text longer than {remove_len} characters")
-
-    # This many rows are left
-    print(f"Rows left: {len(data)}")
 
     return data
 
